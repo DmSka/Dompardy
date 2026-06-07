bcce8a3b64b758b5c0bd1c2380cdde0366b5494d
DmSka <40703147+DmSka@users.noreply.github.com>
2026-06-07 19:09:44 -0400

    Homemade Jeopardy Game
    Homemade Jeopardy Game

---
+++ questions.js
@@ -0,0 +1,60 @@
+const gameData = [
+{
+    category: "Movies",
+    questions: [
+        {
+            points: 100,
+            question: "This movie features dinosaurs on an island.",
+            answer: "Jurassic Park"
+        },
+        {
+            points: 200,
+            question: "",
+            answer: "Cast Away"
+        }
+    ]
+},
+
+{
+    category: "Theme Parks",
+    questions: [
+        {
+            points: 100,
+            question: "Disney's Florida castle park.",
+            answer: "Magic Kingdom"
+        },
+        {
+            points: 200,
+            question: "This Ohio park is home to Millennium Force.",
+            answer: "Cedar Point"
+        }
+    ]
+},
+
+{
+    category: "Science",
+    questions: [
+        {
+            points: 100,
+            question: "The center of an atom.",
+            answer: "Nucleus"
+        }
+    ]
+}
+
+{
+    category: "Go Touch Some Grass",
+    questions: [
+        {
+            points: 100,
+            question: "",
+            answer: ""
+        },
+        {
+            points: 200,
+            question: "",
+            answer: ""
+        }
+    ]
+},
+];
---
+++ script.js
@@ -0,0 +1,61 @@
+const board = document.getElementById("board");
+
+const modal = document.getElementById("questionModal");
+const questionText = document.getElementById("questionText");
+const answerText = document.getElementById("answerText");
+
+const showAnswerButton = document.getElementById("showAnswer");
+const closeButton = document.getElementById("closeQuestion");
+
+let currentAnswer = "";
+
+gameData.forEach(category => {
+
+    const column = document.createElement("div");
+    column.className = "column";
+
+    const categoryName = document.createElement("div");
+    categoryName.className = "category";
+    categoryName.textContent = category.category;
+
+    column.appendChild(categoryName);
+
+    category.questions.forEach(question => {
+
+        const box = document.createElement("div");
+        box.className = "questionBox";
+
+        box.textContent = `$${question.points}`;
+
+        box.addEventListener("click", () => {
+
+            modal.classList.remove("hidden");
+
+            questionText.textContent = question.question;
+            answerText.textContent = "";
+
+            currentAnswer = question.answer;
+
+            box.classList.add("used");
+
+        });
+
+        column.appendChild(box);
+
+    });
+
+    board.appendChild(column);
+
+});
+
+showAnswerButton.addEventListener("click", () => {
+
+    answerText.textContent = currentAnswer;
+
+});
+
+closeButton.addEventListener("click", () => {
+
+    modal.classList.add("hidden");
+
+});
